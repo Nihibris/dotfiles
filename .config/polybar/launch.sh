14b6e6e2158df470ee82accce1fb602111502dbd
Nihibris <kemage99@gmail.com>
2024-04-10 13:09:44 +0200

    update poly

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,8 +1,9 @@
 #polybar-msg cmd quit
 killall -q polybar
 echo "---" | tee -a /tmp/myBar.log
-polybar root 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar left 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar center 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar right 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar root 2>&1 | tee -a /tmp/root.log & disown
+polybar left 2>&1 | tee -a /tmp/left.log & disown
+polybar center 2>&1 | tee -a /tmp/center.log & disown
+polybar right 2>&1 | tee -a /tmp/right.log & disown
+
 echo "Bars launched..."
