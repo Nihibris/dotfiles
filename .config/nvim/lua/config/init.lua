fa0250f5175f1136c5987b3063514338beba25e0
Nihibris <kemage99@gmail.com>
2024-04-12 06:13:51 +0200

    update

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -3,6 +3,8 @@ vim.g.maplocalleader = " "
 vim.opt.number = true
 vim.wo.relativenumber = true
 vim.opt.wrap = false
+vim.opt.clipboard = "unnamed"
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
